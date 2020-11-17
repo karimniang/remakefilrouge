4b238966e1bc74e7e35a1020122fd0d0ed8595e0
karimniang <karim.niang.basse@gmail.com>
2020-11-17 21:07:49 +0000

    again

---
+++ migrations/Version20201117150529.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201117150529 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD deleted TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE user_profil ADD deleted TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP deleted');
+        $this->addSql('ALTER TABLE user_profil DROP deleted');
+    }
+}
